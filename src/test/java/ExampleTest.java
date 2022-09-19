72ef0c937f114988a7542418fd92f84d60485e4f
ciklop <ciklop22@gmail.com>
2022-09-19 22:42:54 +0300

    added second test

---
+++ src/test/java/ExampleTest.java
@@ -0,0 +1,16 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class ExampleTest {
+    @Test
+    void exampleTest0() {
+        Assertions.assertTrue(2>1);
+    }
+        @Test
+        void exampleTest1() {
+            Assertions.assertTrue(3>2);
+}
+}
+
+
+
