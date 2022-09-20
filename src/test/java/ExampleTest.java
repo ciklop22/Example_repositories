dc9bbfe6c52aaa5283c7952ea6484fb428e50720
ciklop <ciklop22@gmail.com>
2022-09-20 18:25:12 +0300

    Fix logic

--- src/test/java/ExampleTest.java
+++ src/test/java/ExampleTest.java
@@ -4,15 +4,15 @@ import org.junit.jupiter.api.Test;
 class ExampleTest {
     @Test
     void exampleTest0() {
-        Assertions.assertTrue(2>1);
+        Assertions.assertTrue(2>=1);
     }
         @Test
         void exampleTest1() {
-            Assertions.assertTrue(3>2);
+            Assertions.assertTrue(3>=2);
 }
     @Test
     void exampleTest2() {
-        Assertions.assertTrue(5>2);
+        Assertions.assertTrue(5>=2);
     }
 }
 
