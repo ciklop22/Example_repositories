a97ffb94965a8525bfcf8adcf5ec55a8bcbec4bf
ciklop <ciklop22@gmail.com>
2022-09-20 21:10:21 +0300

    test

---
+++ src/test/java/ExampleTest.java
@@ -0,0 +1,19 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.Random;
+
+class ExampleTest {
+    @Test
+    void exampleTest0() {
+        Assertions.assertTrue( new Random().nextInt()>1);
+    }
+        @Test
+        void exampleTest1() {
+            Assertions.assertTrue(new Random().nextInt()>2);
+    }
+
+}
+
+
+
