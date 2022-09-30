f96aa967f8b460054116161b7b7c6ce9ef0fac1c
ciklop <ciklop22@gmail.com>
2022-09-30 22:58:07 +0300

    added Junit demo new

---
+++ src/test/java/JunitDemoTest.java
@@ -0,0 +1,38 @@
+import org.junit.jupiter.api.*;
+
+public class JunitDemoTest {
+
+    @BeforeAll
+    static void setUp() {
+        System.out.println("###  @BeforeAll() !");
+
+    }
+    @AfterAll
+    static void tearDown() {
+        System.out.println("###     @AfterAll() !");
+    }
+    @BeforeEach
+    void beforeEachTest() {
+        // open("youtube.com");
+        System.out.println("###        @BeforeEach() !");
+
+    }
+    @AfterEach
+    void afterEachTest() {
+        System.out.println("###        @AfterEach () !");
+
+    }
+
+
+    @Test
+    void simpleTestFirst() {
+        System.out.println("###  @TestFirst() !");
+        Assertions.assertTrue(3>1);
+    }
+
+    @Test
+    void simpleTestSecond() {
+        System.out.println("###  @TestSecond() !");
+        Assertions.assertTrue(3>1);
+    }
+}
